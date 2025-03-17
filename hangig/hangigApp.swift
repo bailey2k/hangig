71399d8ac90be3e13e930c66e0fc98b8ab9bdf62
Bailey Jones <bailey.jones740@gmail.com>
2025-03-17 02:10:57 -0400

    Initial Commit

---
+++ hangig/hangigApp.swift
@@ -0,0 +1,17 @@
+//
+//  hangigApp.swift
+//  hangig
+//
+//  Created by Bailey Jones on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct hangigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hangigTests/hangigTests.swift
@@ -0,0 +1,17 @@
+//
+//  hangigTests.swift
+//  hangigTests
+//
+//  Created by Bailey Jones on 3/17/25.
+//
+
+import Testing
+@testable import hangig
+
+struct hangigTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
